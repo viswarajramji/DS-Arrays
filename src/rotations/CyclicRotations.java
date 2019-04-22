c097c6df0e9052136f64066be9209bb6c59fba7e
Viswarajramji <viswarajramji93@gmail.com>
2019-04-22 21:22:17 +0530

    array cyclic rotations

--- src/rotations/CyclicRotations.java
+++ src/rotations/CyclicRotations.java
@@ -1,20 +1,24 @@
 package rotations;
 
-import java.util.Arrays;
-
 public class CyclicRotations {
-	static void rotate(int arr[]) {
-		int x = arr[arr.length - 1], i;
-		for (i = arr.length - 1; i > 0; i--) {
+	static void cyclic_rotations(int arr[]) {
+		int x = arr[arr.length - 1];
+		for (int i = arr.length - 1; i > 0; i--) {
 			arr[i] = arr[i - 1];
 		}
 		arr[0] = x;
-		System.out.println(Arrays.toString(arr));
+		print_results(arr);
+	}
+
+	static void print_results(int arr[]) {
+		for (int temp : arr) {
+			System.out.print(temp+" ");
+		}
 	}
 
 	public static void main(String[] args) {
-		int arr[]= {1,2,3,4,5};
-		CyclicRotations cr=new CyclicRotations();
-		cr.rotate(arr);
+		int arr[]= {1,2,3,4,5,6,7};
+		cyclic_rotations(arr);
+		
 	}
 }
