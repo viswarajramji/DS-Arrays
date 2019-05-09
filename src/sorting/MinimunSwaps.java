d475932f6492ef5e0a777f529845d7bd8ac58a72
Viswarajramji <viswarajramji93@gmail.com>
2019-05-10 01:48:08 +0530

    sort 0s and 1s

--- src/sorting/MinimunSwaps.java
+++ src/sorting/MinimunSwaps.java
@@ -1,5 +1,35 @@
 package sorting;
 
-public class MinimunSwaps {
+import java.util.Arrays;
 
+public class MinimunSwaps {
+	public static void main(String[] args) {
+		int arr[] =  {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1};
+		int low = 0;
+		int mid = 0;
+		int high = arr.length - 1;
+		while (mid <=high) {
+			switch (arr[mid]) {
+			case 0:
+				int temp = arr[low];
+				arr[low] = arr[mid];
+				arr[mid] = temp;
+				low++;
+				mid++;
+				break;
+			case 1:
+				mid++;
+				break;
+			case 2:
+				temp = arr[high];
+				arr[high] = arr[mid];
+				arr[mid] = temp;
+				high--;
+				break;
+
+			}
+		}
+		
+		System.out.println(Arrays.toString(arr));
+	}
 }
