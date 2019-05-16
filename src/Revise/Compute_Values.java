8d3cb7461c2053345032e19c447ad19d07f45cf5
Viswarajramji <viswarajramji93@gmail.com>
2019-05-17 00:09:08 +0530

    Compute Values prio quie

---
+++ src/Revise/Compute_Values.java
@@ -0,0 +1,26 @@
+package Revise;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Compute_Values {
+	public static void main(String[] args) {
+		int A[] = { 4, 2, 5, 1 };
+		int B[] = { 8, 0, 5, 3 };
+		int k = 3;
+		PriorityQueue<Integer> prio = new PriorityQueue<Integer>(Collections.reverseOrder());
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < B.length; j++) {
+				prio.add(A[i] + B[j]);
+			}
+		}
+
+		int j = 0;
+		while (j < k) {
+			System.out.println(prio.poll());
+			j++;
+		}
+
+	}
+}
